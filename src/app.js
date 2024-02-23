84155396361d50c3a37bf239790642b438607dae
MeSailor <themicroline@gmail.com>
2024-02-23 18:59:56 +0400

    add get handler to send all boards

--- src/app.js
+++ src/app.js
@@ -1,6 +1,26 @@
 const express = require("express");
 const app = express();
+const port = 3003;
 
-app.listen(3000, () => {
-  console.log("Listen on port 3000...");
+app.use(express.json());
+
+app.get("/boards", (req, res) => {
+  try {
+    // get boards from db
+    let boards = [
+      { thumbUrl: "", title: "Alex's board" },
+      { thumbUrl: "", title: "Peter's board" },
+      { thumbUrl: "", title: "Max's board" },
+    ];
+
+    //send boards
+    res.status(200).send(boards);
+  } catch (e) {
+    console.log(e);
+    res.status(500).send({ message: "Sorry, something went wrong..." });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Listen on port ${port}...`);
 });
